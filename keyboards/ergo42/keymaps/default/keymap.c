592f18cd871150b0754b54e45051f3e7c52ff69a
koichi.yanagimoto <koichi.yanagimoto@rakuten.com>
2019-05-29 16:35:59 +0900

    refactor: 💡 change up/down/left/right

--- keyboards/ergo42/keymaps/default/keymap.c
+++ keyboards/ergo42/keymaps/default/keymap.c
@@ -17,14 +17,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | SYMB | LSft |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  | RSft |  =   |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | META | LGUI |   '  |  \   | LAlt | BSpc | Space|   |Enter | RGUI | RAlt | LEFT | UP   | DOWN | RIGHT|
+   * | META | LGUI |   '  |  \   | LAlt | BSpc | Space|   |Enter | RGUI | RAlt |      |      | META | SYMB |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_DELT,  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_GRV, \
-    KC_TAB,   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RBRC, KC_MINS, \
-    MO(SYMB), KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_EQL, \
-    MO(META), KC_LGUI, KC_QUOT, KC_BSLS, KC_LALT, KC_BSPC, KC_SPC, KC_ENT, KC_RGUI,  KC_RALT, KC_LEFT, KC_UP,  KC_DOWN, KC_RGHT \
+    KC_DELT,  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_GRV, \
+    KC_TAB,   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RBRC,  KC_MINS, \
+    MO(SYMB), KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_EQL, \
+    MO(META), KC_LGUI, KC_QUOT, KC_BSLS, KC_LALT, KC_BSPC, KC_SPC, KC_ENT, KC_RGUI,  KC_RALT, XXXXXXX, XXXXXXX, MO(META), MO(SYMB) \
   ),
 
   /* META
@@ -35,32 +35,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | SYMB |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   | F7   |  F8  |  F9  |  F10 | F11  | F12  | =    |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | META | LGUI |      |      | LAlt | Space| BSpc |   |Enter |      |      | LEFT | UP   | DOWN | RIGHT|
+   * | META | LGUI |      |      | LAlt | Space| BSpc |   |Enter |      |      |      |      |      |      |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
     RESET,   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, XXXXXXX, \
     XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, \
-    _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______ \
+    _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
   ),
 
   /* SYMB
    * ,------------------------------------------------.   ,------------------------------------------------.
    * | ESC  |   !  |   @  |   #  |   $  |   %  |  [   |   |  ]   |   ^  |   &  |   *  |   (  |   )  | `    |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
-   * | Del  | LCtrl|      |      |      |      |  (   |   |  )   |      |      |      |      |      | -    |
+   * | Del  | LCtrl|      |      |      |      |  (   |   |  )   |      |      |      |  UP  |      | -    |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
-   * | SYMB |      |      |      |      |      |  {   |   |  }   |      |      |      |      |      | =    |
+   * | SYMB |      |      |      |      |      |  {   |   |  }   |      |      | LEFT | DOWN | RIGHT| =    |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | META | LGUI |   '  |   \  | LAlt | Space| BSpc |   |Enter | RGUI | RAlt | LEFT | UP   | DOWN | RIGHT|
+   * | META | LGUI |   '  |   \  | LAlt | Space| BSpc |   |Enter | RGUI | RAlt |      |      |      | META |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
     KC_ESC,  S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LBRC,    KC_RBRC,    S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, \
-    KC_DEL,  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_9),    S(KC_0),    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_LBRC), S(KC_RBRC), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______ \
+    KC_DEL,  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_9),    S(KC_0),    XXXXXXX, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_LBRC), S(KC_RBRC), XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, _______, \
+    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______ \
   )
 
 };
